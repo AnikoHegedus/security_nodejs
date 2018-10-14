1dc88adf88a258885795c6cd4ea2b6cd81b4ae72
Unknown <millkamill@hotmail.com>
2018-10-14 21:50:51 +0200

    finalize application and add styling

--- server.js
+++ server.js
@@ -2,55 +2,63 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 // Load the MySQL pool connection
-const pool = require(__dirname + "/" + "config.js" );
+const pool = require(__dirname + "/" + "config.js");
+
+// Load static files (ie: CSS)
+app.use('/static', express.static('static'))
 
 // Create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-// Display all users
-app.get('/users', (request, response) => {
-    pool.query('SELECT * FROM users', (error, result) => {
-        if (error) throw error;
- 
-        response.send(result);
-    });
-});
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
+})
 
 app.get('/', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
+    res.sendFile(__dirname + "/" + "index.html");
 })
+var ExpressBrute = require('express-brute');
+var store = new ExpressBrute.MemoryStore(); // stores state locally, don't use this in production
+var bruteforce = new ExpressBrute(store);
+var permutations = require('permutation');
 
 app.post('/process_post', urlencodedParser, function (req, res) {
-    pool.query('SELECT username, password, secret FROM users', (error, result) => {
-        if (error) throw error;
-        response = {
-            username:req.body.username,
-            password:req.body.password
-        };
-
-        if (req.body.password == result[0].password){
-            res.send(result[0].secret);
+    // brute force tries are based on the permutations of the string entered as password
+    var arr = permutations(req.body.password);
+    // iterate through the permutations and make a http request each time
+    for (var i = 0; i < arr.length; i++) {
+        if (sendRequest(req, res)) {
+            res.send("You are logged in");
         } else {
-            res.send("wrong password");
+            res.send("Wrong password");
         }
-        //response = JSON.stringify(response);
-        // req.body.username = joe - end() or send()
-        // result[0].username = admin - send()
-        
+    }
+    res.redirect('/');
+});
+
+function sendRequest(req, res) {
+
+    bruteforce.prevent, // error 429 if we hit this route too often
+    password = "aba";
+    pool.query('SELECT username, password FROM users', (error, result) => {
+        if (error) throw error;
+        password = result[0].password;
+       return password;
     });
-   // Prepare output in JSON format
-   /* response = {
-      first_name:req.body.first_name,
-      last_name:req.body.last_name
-   };
-   console.log(response);
-   res.end(JSON.stringify(response)); */
-})
+
+    // check if any of the permutations is identical to the password
+    var arr = permutations(req.body.password);
+    loggedIn = false;
+    for (var i = 0; i < arr.length; i++) {
+        if (password == arr[i]) {
+            loggedIn = true;
+        }
+    }
+    return loggedIn;
+}
 
 var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
 
 })
