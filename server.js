988b9b17ddd2a79fc3448c00379f7ef9c3d15773
Unknown <millkamill@hotmail.com>
2018-10-17 20:42:48 +0200

    Use bluebird package and promise.all Fix consts and lets

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 // Load the MySQL pool connection
 const pool = require(__dirname + "/" + "config.js");
 
@@ -8,7 +8,7 @@ const pool = require(__dirname + "/" + "config.js");
 app.use('/static', express.static('static'))
 
 // Create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({
+const urlencodedParser = bodyParser.urlencoded({
     extended: false
 })
 
@@ -26,44 +26,55 @@ app.use('/process_post', limiter); */
 
 
 var permutations = require('permutation');
+
 app.post('/process_post', urlencodedParser, function (req, res) {
     // brute force tries are based on the permutations of the string entered as password
-    var arr = permutations(req.body.password);
+    const arr = permutations(req.body.password);
     // iterate through the permutations and make a http request each time
-    for (var i = 0; i < arr.length; i++) {
-        if (sendRequest(req, res)) {
-            res.send("You are logged in");
-        } else {
-            res.send("Wrong password");
-        }
+    const promises = [];
+    for (let i = 0; i < arr.length; ++i) {
+        // try to stop brute force via limiter
+        //app.use(limiter);
+        // make the http calls
+        promises.push(getPromise(arr[i]));
+        Promise.all(promises)
+            .then(value => {
+                if (value[value.length - 1]) {
+                    res.send("You are logged in");
+                }
+            }).then(value => {
+                res.send("Wrong password");
+
+            })
+            .catch(e => {
+                console.log("error: ", e);
+            });
     }
-    res.redirect('/');
 });
 
-function sendRequest(req, res) {
 
-    password = "aba";
-    pool.query('SELECT username, password FROM users', (error, result) => {
-        if (error) throw error;
-        password = result[0].password;
-        return password;
-    });
-
-    // check if any of the permutations is identical to the password
-    var arr = permutations(req.body.password);
-    loggedIn = false;
-    for (var i = 0; i < arr.length; i++) {
-        if (password == arr[i]) {
-            loggedIn = true;
-        }
-    }
-    return loggedIn;
-}
-
-var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
+const server = app.listen(8081, function () {
+    const host = server.address().address
+    const port = server.address().port
 
     console.log("Example app listening at http://%s:%s", host, port)
 
 })
+
+function getPromise(pw) {
+    const Promise = require("bluebird");
+    return new Promise((resolve, reject) => {
+        // get password from database
+        pool.query('SELECT username, password FROM users', (error, result) => {
+            if (error) throw error;
+            const pwFromDB = result[0].password;
+
+            let loggedIn = false;
+            // check if password is ok - pw = input from user
+            if (pw == pwFromDB) {
+                loggedIn = true;
+            }
+            resolve(loggedIn);
+        });
+    });
+}
